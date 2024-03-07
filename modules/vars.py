0143fb95f366f9feeed34ca093c9c3d8ac98f95c
bhuriya12 <162349127+bhuriya12@users.noreply.github.com>
2024-03-07 11:25:01 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "26451206"
 api_hash = "32984406271d6f3945bb536671b143a7"
-bot_token = "6709527547:AAFnCAcJAuEO_z2PfPrh-pIAVG65EZK-45I"
+bot_token = "7178665795:AAFUn9hEM_Wvx3eG-M7A6wXwNkfrAkBbZbU"
