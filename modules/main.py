e84fcdee8284459b02b94985320252652fe647e4
LuckyRajputXD <138707175+LuckyRajputXD@users.noreply.github.com>
2024-03-16 14:26:18 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -170,9 +170,9 @@ async def account_login(bot: Client, m: Message):
                         cmd = f'yt-dlp -o "{name}.pdf" "{url}"'
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
-                        copy = await bot.send_document(chat_id=m.chat.id, document=f'{name1}.pdf', caption=cc1)
+                        copy = await bot.send_document(chat_id=m.chat.id, document=f'{name}.pdf', caption=cc1)
                         count += 1
-                        os.remove(f'{name1}.pdf')
+                        os.remove(f'{name}.pdf')
                     except FloodWait as e:
                         await m.reply_text(str(e))
                         time.sleep(e.x)
