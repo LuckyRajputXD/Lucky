057a85ed3fb67dafd6d35d0f09d71c9ea8155f84
LuckyRajputXD <138707175+LuckyRajputXD@users.noreply.github.com>
2024-03-19 21:32:27 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "3748059"
 api_hash = "f8c9df448f3ba20a900bc2ffc8dae9d5"
-bot_token = "6467851554:AAHBP8nswMz8jZs-cy6C_xxUEdOt0sEttQw"
+bot_token = "7107892208:AAGGmfJrLfNWH1pzFmcBLOJaSgTMvCGug80"
